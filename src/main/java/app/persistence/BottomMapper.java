45f7588deb946345fb9c7cc204648b2570fa80f4
Agnethechr <agnethechr18@gmail.com>
2024-10-22 12:06:48 +0200

    måske lavet bottom og topping mapper

--- src/main/java/app/persistence/BottomMapper.java
+++ src/main/java/app/persistence/BottomMapper.java
@@ -1,4 +1,32 @@
 package app.persistence;
 
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BottomMapper {
+
+    public static Boolean addBottom(int userId, String bottom, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "INSERT INTO cupcake (bottom_name) VALUES (?)";
+        boolean result = false;
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, Integer.parseInt(bottom));
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Error, occured when adding a bottom");
+            } else {
+                result = true;
+            }
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Database error", e.getMessage());
+        }
+        return result;
+    }
 }
--- src/main/java/app/persistence/ToppingMapper.java
+++ src/main/java/app/persistence/ToppingMapper.java
@@ -1,4 +1,32 @@
 package app.persistence;
 
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ToppingMapper {
+
+    public static Boolean addTopping(int userId, String topping, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "INSERT INTO cupcake (topping_name) VALUES (?)";
+        boolean result = false;
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, Integer.parseInt(topping));
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Error, occured when adding a topping");
+            } else {
+                result = true;
+            }
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Database error", e.getMessage());
+        }
+        return result;
+    }
 }
